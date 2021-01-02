e47e3969dbb0d7a2050317794bedf24d8be267bb
Dallas Clarke <dallas.ag.clarke@gmail.com>
2021-01-02 15:27:43 -0800

    Added in React Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 
 // Global Style
 import GlobalStyle from "./components/GlobalStyle";
 // Pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <Nav /> 
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import styled from "styled-components";
 
@@ -6,17 +7,19 @@ const Nav = () => {
   return (
     <StyledNav>
       <h1>
-        <a id="logo" href="#">Capture</a>
+        <Link id="logo" to="/">
+          Capture
+        </Link>
       </h1>
       <ul>
         <li>
-          <a href="#">1. About Us</a>
+          <Link to="/">1. About Us</Link>
         </li>
         <li>
-          <a href="#">2. Our Work</a>
+          <Link to="/work">2. Our Work</Link>
         </li>
         <li>
-          <a href="#">3. Contact Us</a>
+          <Link to="/contact">3. Contact Us</Link>
         </li>
       </ul>
     </StyledNav>
@@ -24,31 +27,31 @@ const Nav = () => {
 };
 
 const StyledNav = styled.nav`
-    min-height: 10vh;
-    display: flex;
-    margin: auto;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1rem 10rem;
-    background: #282828;
+  min-height: 10vh;
+  display: flex;
+  margin: auto;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 10rem;
+  background: #282828;
 
-    a {
-        color: white;
-        text-decoration: none;
-    }
-    ul {
-        display: flex;
-        list-style: none;
-    }
-    #logo {
-        font-size: 1.5rem;
-        font-family: "Lobster", cursive;
-        font-weight: lighter;
-    }
-    li {
-        padding-left: 10rem;
-        position: relative;
-    }
-`
+  a {
+    color: white;
+    text-decoration: none;
+  }
+  ul {
+    display: flex;
+    list-style: none;
+  }
+  #logo {
+    font-size: 1.5rem;
+    font-family: "Lobster", cursive;
+    font-weight: lighter;
+  }
+  li {
+    padding-left: 10rem;
+    position: relative;
+  }
+`;
 
 export default Nav;
