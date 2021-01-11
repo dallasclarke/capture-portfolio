439c7ab30edccbef2c656de17c72f67bc1ee73c7
dallasclarke <dallas.clarke622@gmail.com>
2021-01-11 12:01:02 -0800

    Added animation to aboutSection

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -10,7 +10,7 @@ const FaqSection = () => {
         Any Questions <span>FAQ</span>
       </h2>
       <div className="question">
-        <h4>How Do I Start</h4>
+        <h4>How Do I Start</h4>,
         <div className="answer">
           <p>Lorem ipsum dolor sit amet.</p>
           <p>
