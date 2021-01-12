029f8bf3792e243b5627d66589e056a082f5b013
dallasclarke <dallas.clarke622@gmail.com>
2021-01-11 17:41:08 -0800

    Working on FAQ

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styled from "styled-components";
 import { About } from "../styles";
+import Toggle from "./Toggle";
 
 const FaqSection = () => {
   return (
@@ -9,8 +10,7 @@ const FaqSection = () => {
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
-      <div className="question">
-        <h4>How Do I Start</h4>,
+      <Toggle title="How Do I Start?">
         <div className="answer">
           <p>Lorem ipsum dolor sit amet.</p>
           <p>
@@ -18,41 +18,34 @@ const FaqSection = () => {
             officiis animi quasi, velit eius deserunt.
           </p>
         </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Daily Schedule</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
-            officiis animi quasi, velit eius deserunt.
-          </p>
+      </Toggle>
+      <Toggle title="Daily Schedule">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+              officiis animi quasi, velit eius deserunt.
+            </p>
         </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Payment Methods</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
-            officiis animi quasi, velit eius deserunt.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>What products do you offer?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
-            officiis animi quasi, velit eius deserunt.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
+      </Toggle>
+      <Toggle title="Payment Methods">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+              officiis animi quasi, velit eius deserunt.
+            </p>
+          </div>
+      </Toggle>
+      <Toggle title="What Products Do You Offer?">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+              officiis animi quasi, velit eius deserunt.
+            </p>
+          </div>
+      </Toggle>
     </Faq>
   );
 };
